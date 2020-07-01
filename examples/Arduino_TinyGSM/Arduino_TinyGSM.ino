b0304b918dc726939bfa914272670d1cc28a8cbf
lewis he <lewisxhe@outlook.com>
2020-07-01 11:22:54 +0800

    Modify Arduino_TinyGSM sample code to be compatible with SIM800C, update README

--- examples/Arduino_TinyGSM/Arduino_TinyGSM.ino
+++ examples/Arduino_TinyGSM/Arduino_TinyGSM.ino
@@ -1,27 +1,22 @@
-/**************************************************************
- *
- * This sketch connects to a website and downloads a page.
- * It can be used to perform HTTP/RESTful API calls.
- *
- * TinyGSM Getting Started guide:
- *   https://tiny.cc/tinygsm-readme
- *
- **************************************************************/
 
-// Your GPRS credentials (leave empty, if missing)
-const char apn[]      = ""; // Your APN
-const char gprsUser[] = ""; // User
-const char gprsPass[] = ""; // Password
-const char simPIN[]   = ""; // SIM card PIN code, if any
 
-// TTGO T-Call pin definitions
-#define MODEM_RST            5
-#define MODEM_PWKEY          4
-#define MODEM_POWER_ON       23
-#define MODEM_TX             27
-#define MODEM_RX             26
-#define I2C_SDA              21
-#define I2C_SCL              22
+
+// Please select the corresponding model
+
+// #define SIM800L_IP5306_VERSION_20190610
+// #define SIM800L_AXP192_VERSION_20200327
+// #define SIM800C_AXP192_VERSION_20200609
+
+
+// #define TEST_RING_RI_PIN            //Note will cancel the phone call test
+
+// #define ENABLE_SPI_SDCARD   //Uncomment will test external SD card
+
+// Define the serial console for debug prints, if needed
+// #define DUMP_AT_COMMANDS
+// #define TINY_GSM_DEBUG SerialMon
+
+#include "utilities.h"
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
@@ -29,132 +24,306 @@ const char simPIN[]   = ""; // SIM card PIN code, if any
 #define SerialAT  Serial1
 
 // Configure TinyGSM library
-#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
-#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+#define TINY_GSM_MODEM_SIM800          // Modem is SIM800
+#define TINY_GSM_RX_BUFFER      1024   // Set RX buffer to 1Kb
 
-// Define the serial console for debug prints, if needed
-//#define TINY_GSM_DEBUG SerialMon
-//#define DUMP_AT_COMMANDS
-
-#include <Wire.h>
 #include <TinyGsmClient.h>
-#include "utilities.h"
 
 #ifdef DUMP_AT_COMMANDS
-  #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
-  TinyGsm modem(debugger);
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
 #else
-  TinyGsm modem(SerialAT);
+TinyGsm modem(SerialAT);
 #endif
+
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
 
 
 // Server details
 const char server[] = "vsh.pp.ua";
 const char resource[] = "/TinyGSM/logo.txt";
 
+// Your GPRS credentials (leave empty, if missing)
+const char apn[]      = ""; // Your APN
+const char gprsUser[] = ""; // User
+const char gprsPass[] = ""; // Password
+const char simPIN[]   = ""; // SIM card PIN code, if any
 
 TinyGsmClient client(modem);
 const int  port = 80;
 
-void setup() {
-  // Set console baud rate
-  SerialMon.begin(115200);
-  delay(10);
+/*
+This is just to demonstrate how to use SPI device externally.
+Here we use SD card as a demonstration. In order to maintain versatility,
+I chose three boards with free pins as SPI pins
+*/
+#ifdef ENABLE_SPI_SDCARD
 
-  // Keep power when running from battery
-  Wire.begin(I2C_SDA, I2C_SCL);
-  bool   isOk = setPowerBoostKeepOn(1);
-  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
 
-  // Set-up modem reset, enable, power pins
-  pinMode(MODEM_PWKEY, OUTPUT);
-  pinMode(MODEM_RST, OUTPUT);
-  pinMode(MODEM_POWER_ON, OUTPUT);
+SPIClass SPI1(HSPI);
 
-  digitalWrite(MODEM_PWKEY, LOW);
-  digitalWrite(MODEM_RST, HIGH);
-  digitalWrite(MODEM_POWER_ON, HIGH);
+#define MY_CS       33
+#define MY_SCLK     25
+#define MY_MISO     27
+#define MY_MOSI     26
 
-  // Set GSM module baud rate and UART pins
-  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-  delay(3000);
+void setupSDCard()
+{
+    SPI1.begin(MY_SCLK, MY_MISO, MY_MOSI, MY_CS);
+    //Assuming use of SPI SD card
+    if (!SD.begin(MY_CS, SPI1)) {
+        Serial.println("Card Mount Failed");
+    } else {
+        Serial.println("SDCard Mount PASS");
+        String size = String((uint32_t)(SD.cardSize() / 1024 / 1024)) + "MB";
+        Serial.println(size);
+    }
+}
+#else
+#define setupSDCard()
+#endif
 
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  SerialMon.println("Initializing modem...");
-  modem.restart();
-  // Or, use modem.init() if you don't need the complete restart
 
-  String modemInfo = modem.getModemInfo();
-  SerialMon.print("Modem: ");
-  SerialMon.println(modemInfo);
+void setupModem()
+{
+#ifdef MODEM_RST
+    // Keep reset high
+    pinMode(MODEM_RST, OUTPUT);
+    digitalWrite(MODEM_RST, HIGH);
+#endif
 
-  // Unlock your SIM card with a PIN if needed
-  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(simPIN);
-  }
+    pinMode(MODEM_PWRKEY, OUTPUT);
+    pinMode(MODEM_POWER_ON, OUTPUT);
+
+    // Turn on the Modem power first
+    digitalWrite(MODEM_POWER_ON, HIGH);
+
+    // Pull down PWRKEY for more than 1 second according to manual requirements
+    digitalWrite(MODEM_PWRKEY, HIGH);
+    delay(100);
+    digitalWrite(MODEM_PWRKEY, LOW);
+    delay(1000);
+    digitalWrite(MODEM_PWRKEY, HIGH);
+
+    // Initialize the indicator as an output
+    pinMode(LED_GPIO, OUTPUT);
+    digitalWrite(LED_GPIO, LED_OFF);
 }
 
-void loop() {
-  SerialMon.print("Waiting for network...");
-  if (!modem.waitForNetwork(240000L)) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
-  }
-  SerialMon.println(" OK");
+void turnOffNetlight()
+{
+    SerialMon.println("Turning off SIM800 Red LED...");
+    modem.sendAT("+CNETLIGHT=0");
+}
 
-  if (modem.isNetworkConnected()) {
-    SerialMon.println("Network connected");
-  }
+void turnOnNetlight()
+{
+    SerialMon.println("Turning on SIM800 Red LED...");
+    modem.sendAT("+CNETLIGHT=1");
+}
 
-  SerialMon.print(F("Connecting to APN: "));
-  SerialMon.print(apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
-  }
-  SerialMon.println(" OK");
+void setup()
+{
+    // Set console baud rate
+    SerialMon.begin(115200);
 
-  SerialMon.print("Connecting to ");
-  SerialMon.print(server);
-  if (!client.connect(server, port)) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
-  }
-  SerialMon.println(" OK");
+    delay(10);
 
-  // Make a HTTP GET request:
-  SerialMon.println("Performing HTTP GET request...");
-  client.print(String("GET ") + resource + " HTTP/1.1\r\n");
-  client.print(String("Host: ") + server + "\r\n");
-  client.print("Connection: close\r\n\r\n");
-  client.println();
-
-  unsigned long timeout = millis();
-  while (client.connected() && millis() - timeout < 10000L) {
-    // Print available data
-    while (client.available()) {
-      char c = client.read();
-      SerialMon.print(c);
-      timeout = millis();
+    // Start power management
+    if (setupPMU() == false) {
+        Serial.println("Setting power error");
     }
-  }
-  SerialMon.println();
 
-  // Shutdown
+    setupSDCard();
 
-  client.stop();
-  SerialMon.println(F("Server disconnected"));
+    // Some start operations
+    setupModem();
 
-  modem.gprsDisconnect();
-  SerialMon.println(F("GPRS disconnected"));
+    // Set GSM module baud rate and UART pins
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+}
 
-  // Do nothing forevermore
-  while (true) {
+void loop()
+{
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    SerialMon.println("Initializing modem...");
+    modem.restart();
+
+    // Turn off network status lights to reduce current consumption
+    turnOffNetlight();
+
+    // The status light cannot be turned off, only physically removed
+    //turnOffStatuslight();
+
+    // Or, use modem.init() if you don't need the complete restart
+    String modemInfo = modem.getModemInfo();
+    SerialMon.print("Modem: ");
+    SerialMon.println(modemInfo);
+
+    // Unlock your SIM card with a PIN if needed
+    if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+        modem.simUnlock(simPIN);
+    }
+
+    SerialMon.print("Waiting for network...");
+    if (!modem.waitForNetwork(240000L)) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" OK");
+
+    // When the network connection is successful, turn on the indicator
+    digitalWrite(LED_GPIO, LED_ON);
+
+    if (modem.isNetworkConnected()) {
+        SerialMon.println("Network connected");
+    }
+
+    SerialMon.print(F("Connecting to APN: "));
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" OK");
+
+    SerialMon.print("Connecting to ");
+    SerialMon.print(server);
+    if (!client.connect(server, port)) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" OK");
+
+    // Make a HTTP GET request:
+    SerialMon.println("Performing HTTP GET request...");
+    client.print(String("GET ") + resource + " HTTP/1.1\r\n");
+    client.print(String("Host: ") + server + "\r\n");
+    client.print("Connection: close\r\n\r\n");
+    client.println();
+
+    unsigned long timeout = millis();
+    while (client.connected() && millis() - timeout < 10000L) {
+        // Print available data
+        while (client.available()) {
+            char c = client.read();
+            SerialMon.print(c);
+            timeout = millis();
+        }
+    }
+    SerialMon.println();
+
+    // Shutdown
+    client.stop();
+    SerialMon.println(F("Server disconnected"));
+
+    modem.gprsDisconnect();
+    SerialMon.println(F("GPRS disconnected"));
+
+
+    // DTR is used to wake up the sleeping Modem
+    // DTR is used to wake up the sleeping Modem
+    // DTR is used to wake up the sleeping Modem
+#ifdef MODEM_DTR
+    bool res;
+
+    modem.sleepEnable();
+
+    delay(100);
+
+    // test modem response , res == 0 , modem is sleep
+    res = modem.testAT();
+    Serial.print("SIM800 Test AT result -> ");
+    Serial.println(res);
+
     delay(1000);
-  }
+
+    Serial.println("Use DTR Pin Wakeup");
+    pinMode(MODEM_DTR, OUTPUT);
+    //Set DTR Pin low , wakeup modem .
+    digitalWrite(MODEM_DTR, LOW);
+
+
+    // test modem response , res == 1 , modem is wakeup
+    res = modem.testAT();
+    Serial.print("SIM800 Test AT result -> ");
+    Serial.println(res);
+
+#endif
+
+
+#ifdef TEST_RING_RI_PIN
+#ifdef MODEM_RI
+    // Swap the audio channels
+    SerialAT.print("AT+CHFA=1\r\n");
+    delay(2);
+
+    //Set ringer sound level
+    SerialAT.print("AT+CRSL=100\r\n");
+    delay(2);
+
+    //Set loud speaker volume level
+    SerialAT.print("AT+CLVL=100\r\n");
+    delay(2);
+
+    // Calling line identification presentation
+    SerialAT.print("AT+CLIP=1\r\n");
+    delay(2);
+
+    //Set RI Pin input
+    pinMode(MODEM_RI, INPUT);
+
+    Serial.println("Wait for call in");
+    //When is no calling ,RI pin is high level
+    while (digitalRead(MODEM_RI)) {
+        Serial.print('.');
+        delay(500);
+    }
+    Serial.println("call in ");
+
+    //Wait 10 seconds for the bell to ring
+    delay(10000);
+
+    //Accept call
+    SerialAT.println("ATA");
+
+
+    delay(10000);
+
+    // Wait ten seconds, then hang up the call
+    SerialAT.println("ATH");
+#endif  //MODEM_RI
+#endif  //TEST_RING_RI_PIN
+
+    // Make the LED blink three times before going to sleep
+    int i = 3;
+    while (i--) {
+        digitalWrite(LED_GPIO, LED_ON);
+        delay(500);
+        digitalWrite(LED_GPIO, LED_OFF);
+        delay(500);
+    }
+
+    //After all off
+    modem.poweroff();
+
+    SerialMon.println(F("Poweroff"));
+
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+    esp_deep_sleep_start();
+
+    /*
+    The sleep current using AXP192 power management is about 500uA,
+    and the IP5306 consumes about 1mA
+    */
 }
