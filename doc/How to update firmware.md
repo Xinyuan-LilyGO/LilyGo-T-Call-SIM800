164743968ef7a172a351c79bd8cfd736a607ecb2
lewisxhe <lewishe@outlook.com>
2021-02-25 12:02:31 +0800

    Add built-in firmware update instructions

---
+++ doc/How to update firmware.md
@@ -0,0 +1,24 @@
+
+
+# Step 
+### 1. First, you need to update the ESP32's built-in firmware, open the `UpdateFirmware` example, compile and burn it into the board. 
+
+### 2. Unzip `SIM800_Series_download_Tools_Customer_v1.23.rar` 
+
+### 3. Open `Flash_tool.exe`
+![](image/1.png)
+
+### 4. Select the relative firmware and port according to the diagram 
+![](image/2.png)
+
+### 5. Wait for the board indicator to light up, then click the `Start Download` button, and wait for it to finish 
+![](image/3.png)
+
+# Firmware Link
+- [Flash Tools](https://1drv.ms/u/s!AmbpOqVezk5drRlUsrka6VY5OJds?e=daUEOB)
+- [SIM800L FW](https://1drv.ms/u/s!AmbpOqVezk5drRsAB6lqCZs4HvWR?e=iujjDh)
+- [SIM800C FW](https://1drv.ms/u/s!AmbpOqVezk5drRosZ9mXFF-RqYbj?e=i4Wld8)
+
+
+
+
---
+++ examples/UpdateFirmware/UpdateFirmware.ino
@@ -0,0 +1,56 @@
+// Please select the corresponding model
+
+// #define SIM800L_IP5306_VERSION_20190610
+// #define SIM800L_AXP192_VERSION_20200327
+// #define SIM800C_AXP192_VERSION_20200609
+// #define SIM800L_IP5306_VERSION_20200811
+
+#include <Arduino.h>
+#include "utilities.h"
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to the module)
+#define SerialAT  Serial1
+
+void setupModem()
+{
+#ifdef MODEM_RST
+    // Keep reset high
+    pinMode(MODEM_RST, OUTPUT);
+    digitalWrite(MODEM_RST, HIGH);
+#endif
+
+    pinMode(MODEM_PWRKEY, OUTPUT);
+    pinMode(MODEM_POWER_ON, OUTPUT);
+
+    // Turn on the Modem power first
+    digitalWrite(MODEM_POWER_ON, HIGH);
+
+    // Initialize the indicator as an output
+    pinMode(LED_GPIO, OUTPUT);
+    digitalWrite(LED_GPIO, LED_ON);
+}
+
+
+void setup()
+{
+    // Set console baud rate
+    SerialMon.begin(115200);
+
+    // Set GSM module baud rate and UART pins
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+
+    // Some start operations
+    setupModem();
+}
+
+void loop()
+{
+    while (SerialAT.available()) {
+        SerialMon.write(SerialAT.read());
+    }
+    while (SerialMon.available()) {
+        SerialAT.write(SerialMon.read());
+    }
+}
